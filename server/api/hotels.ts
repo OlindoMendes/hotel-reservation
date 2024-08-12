669394f3ce0c4563038312a2764cf866460bdae2
Tolobinha <xtrubulho@protonmail.com>
2024-08-12 01:30:44 -0300

    merge

--- server/api/hotels.ts
+++ server/api/hotels.ts
@@ -3,7 +3,6 @@ import data from "../hotels.json"
 export default defineEventHandler(async () => {
     // ... Do whatever you want here
     return {
-        data: data,
-        
+        data
     }
 })
