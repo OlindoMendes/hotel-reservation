5e31e961fac147f85bc97e1b0bb3e3c50bf0f050
Tolobinha <xtrubulho@protonmail.com>
2024-08-12 01:34:33 -0300

    commit merge

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", '@pinia/nuxt',],
+  modules: ["@nuxtjs/tailwindcss", '@pinia/nuxt', '@nuxt/test-utils/module',],
   build: {
     hotMiddleware: {
       client: {
---
+++ server/api/test.ts
@@ -0,0 +1,7 @@
+export default defineEventHandler(async () => {
+    // ... Do whatever you want here
+    return {
+        data: "test"
+        
+    }
+})
